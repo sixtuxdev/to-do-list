97bc6c7504bc160c8c69a722e504531672da8568
Sixto José Romero Martínez <desarrolloglo.sixto@outlook.com>
2026-05-01 00:56:20 -0500

    Se aplican cambios para configuración de Firebase y Remote Config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "DEMO_API_KEY",
-    authDomain: "demo-todo-app.firebaseapp.com",
-    projectId: "demo-todo-app",
-    storageBucket: "demo-todo-app.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "1:123456789:web:abcdef123456"
+    apiKey: "AIzaSyAJ1lwnwOJIQn9gkeMW2l_cT62r3dfdD8Y",
+    authDomain: "to-do-list-483db.firebaseapp.com",
+    projectId: "to-do-list-483db",
+    storageBucket: "to-do-list-483db.firebasestorage.app",
+    messagingSenderId: "256781483356",
+    appId: "1:256781483356:web:44a3a4d8677057c9719599"
   }
 };
